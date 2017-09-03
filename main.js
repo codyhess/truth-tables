77157865dc7cb111600d1585f388ec4315dea72b
Cody Hess <cody.hess@gmail.com>
2017-09-02 21:46:15 -0400

    listens for touches
    - also fixed randomizer bug

--- main.js
+++ main.js
@@ -13,21 +13,34 @@ function flip() {
 listen('DOMContentLoaded', function() {
   console.log('hello truth.js');
 
-  let not = Math.random < .5 ? 'A' : 'B';
-  let op = Math.random < .5 ? '&&' : '||';
+  let not = Math.random() < .5 ? 'A' : 'B';
+  console.log(not);
+  let op = Math.random() < .5 ? '&&' : '||';
 
   cols[0] = getID('not');
   cols[1] = getID('combine');
   cols[2] = getID('hellmode');
+  console.log(not);
 
   // get random headers
   ((el, str) => {
     el.innerText = str;
   })(cols[0].children[0], '!' + not);
+  console.log(not);
 
   ((el) => {
     var a = not === 'A' ? '!A' : 'A';
     var b = not === 'B' ? '!B' : 'B';
     el.innerText = a + ' ' + op + ' ' + b;
   })(cols[1].children[0]);
+
+  listen('touchend', (event) => {
+    let t = event.target;
+    if (!(t.dataset.type === 'toggle')) { return; }
+    if (t.innerText !== 'true') {
+      t.innerText = 'true';
+    } else {
+      t.innerText = 'false';
+    }
+  });
 });
